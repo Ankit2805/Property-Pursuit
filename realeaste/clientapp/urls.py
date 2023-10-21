ca8932efb2c6d55491d64db414eb3a2a57bfc668
Spectrics Solutions <64589128+SpectricsSolutions@users.noreply.github.com>
2023-10-21 14:12:02 +0530

    code

--- realeaste/clientapp/urls.py
+++ realeaste/clientapp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from  . import views
 
 urlpatterns = [
-    
+    path('',views.landing,name='landingpage'),
+    path('contact/',views.contact,name='contact')
+
 ]
--- realeaste/clientapp/views.py
+++ realeaste/clientapp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+def landing(request):
+    return render(request,'index.html')
+
+def contact(request):
+    return render(request,'contact.html')
